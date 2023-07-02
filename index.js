00821210c20e12439210adfa3bcc2b118fe37029
sahDipti <123860643+sahDipti@users.noreply.github.com>
2023-07-02 22:55:15 +0545

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var randomDice1=Math.floor(Math.random()*6)+1;
 var randomDice2=Math.floor(Math.random()*6)+1;
+var Dice1="./images/dice"+randomDice1+".png";
+var Dice2="./images/dice"+randomDice2+".png";
+
 if(randomDice1>randomDice2){
     document.querySelector("h1").textContent="Player1 Winner!!";
 }else if(randomDice1==randomDice2){
@@ -7,42 +10,6 @@ if(randomDice1>randomDice2){
 }else{
     document.querySelector("h1").textContent="Player2 Winner!!";
 }
-//dice1;
-if(randomDice1==1){
-    document.querySelector(".img1").setAttribute("src","./images/dice1.png");
-}
-else if(randomDice1==2){
-    document.querySelector(".img1").setAttribute("src","./images/dice2.png");
-}
-else if(randomDice1==3){
-    document.querySelector(".img1").setAttribute("src","./images/dice3.png");
-}
-else if(randomDice1==4){
-    document.querySelector(".img1").setAttribute("src","./images/dice4.png");   
-}
-else if(randomDice1==5){
-    document.querySelector(".img1").setAttribute("src","./images/dice5.png");    
-}
-else if(randomDice1==6){
-    document.querySelector(".img1").setAttribute("src","./images/dice6.png");   
-}
 
-//dice 2
-if(randomDice2==1){
-    document.querySelector(".img2").setAttribute("src","./images/dice1.png");
-}
-else if(randomDice2==2){
-    document.querySelector(".img2").setAttribute("src","./images/dice2.png");
-}
-else if(randomDice2==3){
-    document.querySelector(".img2").setAttribute("src","./images/dice3.png");
-}
-else if(randomDice2==4){
-    document.querySelector(".img2").setAttribute("src","./images/dice4.png");   
-}
-else if(randomDice2==5){
-    document.querySelector(".img2").setAttribute("src","./images/dice5.png");    
-}
-else if(randomDice2==6){
-    document.querySelector(".img2").setAttribute("src","./images/dice6.png");   
-}
+document.querySelector(".img1").setAttribute("src",Dice1);
+document.querySelector(".img2").setAttribute("src",Dice2);
